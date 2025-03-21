81a5ad1d5d34ba869653c1ebff16fc007e3f59f9
ry-sugimoto <r.snowman.no2@gmail.com>
2025-03-21 12:49:57 +0900

    add: cors設定

--- web/config/application.rb
+++ web/config/application.rb
@@ -28,5 +28,16 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << Rails.root.join("app/services")
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
