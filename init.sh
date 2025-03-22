0612c312b45f0b4e7fd4e52b05fcc0d06bccad12
ry-sugimoto <r.snowman.no2@gmail.com>
2025-03-22 09:52:30 +0900

    add: 手順書作成

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
+cp ./web/.env.sample ./web/.env
 docker compose build
 docker compose run --rm web rails db:create
 docker compose run --rm web rails db:migrate
-docker compose up -d
-cp ./web/.env.sample ./web/.env
